2e9dc68191f704a04a36878913b2981b1e8d3e9c
Chris Taylor <chris@christaylordeveloper.co.uk>
2022-12-09 13:43:10 +0100

    Convert tabs to spaces.

--- post-revision-date.php
+++ post-revision-date.php
@@ -6,15 +6,15 @@
  */
 
 function show_last_updated( $content ) {
-	$args = array(
+    $args = array(
         'posts_per_page' => '1',
-		'orderby' => 'post_date_gmt',
-		'order'   => 'DESC',
-	);
+        'orderby' => 'post_date_gmt',
+        'order'   => 'DESC',
+    );
 
-	global $post;
-	$latest_version = wp_get_post_revisions( $post, $args );
-	$revision_time = array_pop( $latest_version )->post_date_gmt;
+    global $post;
+    $latest_version = wp_get_post_revisions( $post, $args );
+    $revision_time = array_pop( $latest_version )->post_date_gmt;
     $revision_date_html = '<p class="last-updated">Updated: ' . date( 'd F Y', strtotime( $revision_time ) ) . ' </p>';
     return $revision_date_html . $content;
 }
